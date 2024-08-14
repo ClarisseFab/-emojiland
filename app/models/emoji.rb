e0541ffcb8b70e59039c1066b4e33957fc0a68cf
Chloe Bourgeois <chloebourgeois69@gmail.com>
2024-08-14 15:59:26 +0200

    filter créer,category dropdown,user in form enlevé, bouton redirigé

--- app/models/emoji.rb
+++ app/models/emoji.rb
@@ -1,4 +1,10 @@
 class Emoji < ApplicationRecord
+
+  acts_as_taggable_on :tags
+  # acts_as_taggable_on :skills, :interests
+  #You can also configure multiple tag types per model
+
+
   belongs_to :user
   has_many :bookings, dependent: :destroy
 
